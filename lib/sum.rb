1bf66201cc265887206f7b875c6692a315a8775b
Brent Gaither <brent.w.gaither@gmail.com>
2018-11-24 17:06:31 -0700

    Implement times for a sum

--- lib/sum.rb
+++ lib/sum.rb
@@ -19,7 +19,11 @@ class Sum < Expression
   end
 
   def plus(addend)
-    nil
+    Sum.new(self, addend)
+  end
+
+  def times(multiplier)
+    Sum.new(augend.times(multiplier), addend.times(multiplier))
   end
 
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -12,6 +12,16 @@ RSpec.describe TddExamples do
       expect(Money.dollar(10)==five.times(2)).to be true
       expect(Money.dollar(15)==five.times(3)).to be true
     end
+
+    it 'should reduce' do
+      five_bucks = Money.dollar(5)
+      ten_francs = Money.franc(10)
+      bank = Bank.new
+      bank.add_rate("CHF", "USD", 2)
+      sum = Sum.new(five_bucks, ten_francs).times(2)
+      result = bank.reduce(sum, "USD")
+      expect(result == Money.dollar(20)).to be true
+    end
   end
 
   it 'should compare equal objects' do
@@ -46,6 +56,11 @@ RSpec.describe TddExamples do
       expect(result.augend).to eq five
       expect(result.addend).to eq five
     end
+
+    it 'should add the same currency' do
+      sum = Money.dollar(1).plus(Money.dollar(1))
+      expect(sum.is_a? Money)
+    end
   end
 
   context 'conversions' do
@@ -56,6 +71,18 @@ RSpec.describe TddExamples do
       bank.add_rate("CHF", "USD", 2)
       result = bank.reduce(five_bucks.plus(ten_francs), "USD")
       expect(result == Money.dollar(10)).to be true
+    end
+  end
+
+  context 'sum' do
+    it 'sums money' do
+      five_bucks = Money.dollar(5)
+      ten_francs = Money.franc(10)
+      bank = Bank.new
+      bank.add_rate("CHF", "USD", 2)
+      sum = Sum.new(five_bucks, ten_francs).plus(five_bucks)
+      result = bank.reduce(sum, "USD")
+      expect(result == Money.dollar(15)).to be true
     end
   end
 end
