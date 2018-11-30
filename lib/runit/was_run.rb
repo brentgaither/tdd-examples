5981fd8c7ce1434f335254a9f6c4401b73355f99
Brent Gaither <brent.w.gaither@gmail.com>
2018-11-30 16:11:48 -0700

    extract a test case class and assert the methods ran

--- lib/runit/was_run.rb
+++ lib/runit/was_run.rb
@@ -1,8 +1,23 @@
-class WasRun
+class TestCase
+  attr_accessor :name
+
+  def initialize name
+    self.name = name
+  end
+
+  def run
+    method = self.name
+    self.send(method)
+  end
+end
+
+class WasRun < TestCase
 
   attr_accessor :was_run
-  def initialize(name)
+
+  def initialize name
     self.was_run = nil
+    super name
   end
 
   def test_method()
@@ -11,8 +26,17 @@ class WasRun
 end
 
 
+def assert &block
+  raise RuntimeError unless yield
+end
 
-test = WasRun.new "testMethod"
-p test.was_run
-test.test_method
-p test.was_run
+class TestCaseTest < TestCase
+  def test_running
+    test = WasRun.new "test_method"
+    assert {!test.was_run}
+    test.run
+    assert {test.was_run}
+  end
+end
+
+TestCaseTest.new("test_running").run
